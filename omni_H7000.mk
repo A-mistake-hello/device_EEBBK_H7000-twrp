bd029613ac9c44ff7d1a9d70a34a0294fbc70265
A-mistake-hello <3329989998@qq.com>
2025-06-06 18:01:54 +0800

    Update omni_H7000.mk

--- omni_H7000.mk
+++ omni_H7000.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore=sdm660 \
